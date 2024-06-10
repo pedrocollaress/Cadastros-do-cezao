f0b847afa58110d5c57745c17712ae74797cc3ec
Matheus <matheusdebortolisilva@gmail.com>
2024-06-10 20:17:18 -0300

    Adicionando a funcionalidade de registro de pessoas

--- src/Pessoa.java
+++
@@ -1,9 +0,0 @@
-public class Pessoa {
-    String nome;
-    String sobrenome;
-
-    public Pessoa(String nome, String sobrenome){
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-    }
-}
