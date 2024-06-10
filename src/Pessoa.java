26aa04ee0af6fa760eb97ee37d71b58576cc06d9
Pedro <pedro.collares05@gmail.com>
2024-06-10 19:52:21 -0300

    criada a classe pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Pessoa {
+public class Pessoa {
+    String nome;
+    String sobrenome;
+
+    public Pessoa(String nome, String sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
 }
