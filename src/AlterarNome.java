9e4cf1bbacaabc8362ce5c9234b5343267cc3a21
MatheusDBS <169103477+MatheusDBS@users.noreply.github.com>
2024-06-10 21:24:02 -0300

    Método Alterar nome

--- src/AlterarNome.java
+++ src/AlterarNome.java
@@ -1,6 +1,13 @@
 public class AlterarNome {
-public static void alteraNome(){
-    System.out.println("Qual nome deseja alterar?");
-}
+public void alterarPessoa(String nome, Pessoa pessoaAtualizada) {
+        for (Pessoa pessoa : pessoas) {
+            if (pessoa.getNome().equals(nome)) {
+                pessoa.setNome(pessoaAtualizada.getNome());
+                pessoa.setIdade(pessoaAtualizada.getIdade());
+                System.out.println("Pessoa alterada com sucesso!");
+                return;
+            }
+        }
+        System.out.println("Pessoa não encontrada.");
 
 }
