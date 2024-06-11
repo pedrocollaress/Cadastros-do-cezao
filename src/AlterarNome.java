75ad98ce9c72ed3b53759c8d4635e8ae44081a82
Matheus <matheusdebortolisilva@gmail.com>
2024-06-10 21:03:29 -0300

    Adicionando método de excluir pessoas

---
+++ src/AlterarNome.java
@@ -0,0 +1,6 @@
+public class AlterarNome {
+public static void alteraNome(){
+    System.out.println("Qual nome deseja alterar?");
+}
+
+}
--- src/RegistroDePessoas.java
+++ src/RegistroDePessoas.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
 public class RegistroDePessoas {
-    private ArrayList<Pessoa> pessoas;
+    private ArrayList<AddPessoa> pessoas;
 
     public RegistroDePessoas() {
         this.pessoas = new ArrayList<>();
     }
 
-    public void adicionarPessoa(Pessoa pessoa) {
+    public void adicionarPessoa(AddPessoa pessoa) {
         pessoas.add(pessoa);
         System.out.println("Pessoa adicionada com sucesso!");
     }
@@ -16,7 +16,7 @@ public class RegistroDePessoas {
         if (pessoas.isEmpty()) {
             System.out.println("Nenhuma pessoa registrada.");
         } else {
-            for (Pessoa pessoa : pessoas) {
+            for (AddPessoa pessoa : pessoas) {
                 System.out.println(pessoa);
             }
         }
